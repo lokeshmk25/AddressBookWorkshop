acc2ecbf61f9367195df183d71507aa002890d2c
lokeshmk25 <lokeshmk2211@gmail.com>
2021-10-11 08:38:40 +0530

    Added:Welcome message

---
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+/*
+@author=Lokesh
+ */
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book ");
+    }
+}
